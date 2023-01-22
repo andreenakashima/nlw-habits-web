fb1e4c74ce4ade62f58a13b187d1941e532ece89
andreenakashima <andreenakashima@gmail.com>
2023-01-22 17:51:49 -0300

    Aula 5

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -9,7 +9,7 @@ export function ProgressBar(props: ProgressBarProps) {
 				role="progressbar"
 				aria-label="Progresso de hábitos completos nesse dia"
 				aria-valuenow={props.progress}
-				className="h-3 rounded-xl bg-violet-600 w-2/4"
+				className="h-3 rounded-xl bg-violet-600 w-2/4 transition-all"
 				style={{ width: `${props.progress}%` }}
 			/>
 		</div>
